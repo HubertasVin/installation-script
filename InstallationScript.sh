9b02d23fc8791fc82e76973f1a3c2906c3d27d31
HubertasVin <hubertas2003@gmail.com>
2023-01-21 16:28:22 +0200

    Patobulinta Debian instaliacija, pridėta TrueType šriftų instaliacija

--- InstallationScript.sh
+++ InstallationScript.sh
@@ -13,20 +13,29 @@ if [ `which apt` ]; then	# App DEBIAN
 	sudo apt clean
 	sudo apt autoremove
 
+	# Add the flathub repository
 	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 	wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 	sudo dpkg -i packages-microsoft-prod.deb
 	rm packages-microsoft-prod.deb
 
+	# Add VS Code, so that it can be installed using apt install
 	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 	rm -f packages.microsoft.gpg
 
+	# Install MS Teams
 	curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-archive-keyring.gpg
 	sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
 	sudo apt update
+
+	# Install TrueType fonts
+	wget http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb
+	sudo dpkg -i ttf-mscorefonts-installer_3.6_all.deb
+	sudo fc-cache -f -v 	# Refresh fonts cache
+	rm ttf-mscorefonts-installer_3.6_all.deb
 
 	aptApps=(ubuntu-restricted-extras snapd gpg vim xclip gdb gnome-common steam-installer code piper npm apt-transport-https code dosbox gnome-tweaks balena-etcher-electron qbittorrent lutris gimp xdotool dotnet-sdk-7.0 aspnetcore-runtime-7.0 dotnet-runtime-7.0 neofetch docbook-xml teams intltool autoconf-archive itstool docbook-xsl yelp-tools glib2-docs python-pygments gtk-doc-tools sddm)
 	snapApps=(starship spotify mc-installer discord vlc)
@@ -39,6 +48,15 @@ if [ `which apt` ]; then	# App DEBIAN
 	do
 		sudo snap install ${snapApps[$i]}
 	done
+
+	#TODO ---- Setup sddm ----
+	if [ `systemctl status display-manager | head -n1 | awk '{print $2;}'` != 'sddm.service' ]; then
+		echo 'Pakeičiamas display manager į sddm'
+		systemctl disable `systemctl status display-manager | head -n1 | awk '{print $2;}'`
+		systemctl enable sddm
+		sddm --example-config | sed 's/^DisplayCommand/# &/' | sed 's/^DisplayStopCommand/# &/' | sed '/Current=/s/$/plasma-chili/' | sudo tee /etc/sddm.conf > /dev/null
+		sudo mv plasma-chili /usr/share/sddm/themes/
+	fi
 
 	echo 'xrandr --output DP-1 --mode 1920x1080 --rate 144' >> ~/.bashrc
 	echo 'alias xr144="xrandr --output DP-1 --mode 1920x1080 --rate 144"' >> ~/.bashrc
