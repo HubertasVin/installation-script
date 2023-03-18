909921cfdf4f6b8883f1ee54e5d08487311a7548
HubertasVin <hubertas2003@gmail.com>
2023-03-18 19:07:46 +0200

    Further script updates

--- InstallationScript.sh
+++ InstallationScript.sh
@@ -3,41 +3,50 @@
 
 scriptLoc=$(pwd)
 
-# ?----------------------------------------------
-# ?|                  DEBIAN                    |
-# ?----------------------------------------------
+# ?┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
+# ?│                   DEBIAN                   │
+# ?┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
 if [ `which apt` ]; then	# App DEBIAN
+
+	#TODO ---- Post-installation necessary commands ----
+	PROMPT_COMMAND="Running Post-Installation System Updates"
 	sudo apt update
 	sudo apt upgrade
 	sudo apt install -y flatpak wget
 	sudo apt clean
 	sudo apt autoremove
 
-	# Add the flathub repository
+	#TODO ---- Add the flathub repository ----
+	PROMPT_COMMAND="Setting Up Flatpak"
 	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 	wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 	sudo dpkg -i packages-microsoft-prod.deb
 	rm packages-microsoft-prod.deb
 
-	# Add VS Code, so that it can be installed using apt install
+	#TODO ---- Add VSCode, so that it can be installed using apt install ----
+	PROMPT_COMMAND="Setting Up VS Code"
 	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 	rm -f packages.microsoft.gpg
 
-	# Install MS Teams
+	#TODO ---- Install MS Teams ----
+	PROMPT_COMMAND="Setting Up MS Teams"
 	curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-archive-keyring.gpg
 	sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
 	sudo apt update
 
-	# Install TrueType fonts
+	#TODO ---- Install TrueType fonts ----
+	PROMPT_COMMAND="Installing TrueType Fonts"
 	wget http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb
 	sudo dpkg -i ttf-mscorefonts-installer_3.6_all.deb
 	sudo fc-cache -f -v 	# Refresh fonts cache
 	rm ttf-mscorefonts-installer_3.6_all.deb
 
-	aptApps=(ubuntu-restricted-extras snapd gpg vim xclip gdb gnome-common steam-installer code piper npm apt-transport-https code dosbox gnome-tweaks balena-etcher-electron qbittorrent lutris gimp xdotool dotnet-sdk-7.0 aspnetcore-runtime-7.0 dotnet-runtime-7.0 neofetch docbook-xml teams intltool autoconf-archive itstool docbook-xsl yelp-tools glib2-docs python-pygments gtk-doc-tools sddm)
+	#TODO ---- Install necessary applications ----
+	PROMPT_COMMAND="Installing Necessary Applications"
+	aptApps=(ubuntu-restricted-extras snapd gpg vim xclip gdb gnome-common steam-installer code piper npm apt-transport-https code dosbox gnome-tweaks balena-etcher-electron qbittorrent lutris gimp xdotool dotnet-sdk-7.0 aspnetcore-runtime-7.0 dotnet-runtime-7.0 neofetch docbook-xml teams intltool autoconf-archive itstool docbook-xsl yelp-tools glib2-docs python-pygments gtk-doc-tools sddm dconf-editor)
 	snapApps=(starship spotify mc-installer discord vlc)
 	
 	for i in ${!aptApps[@]}
@@ -49,6 +58,7 @@ if [ `which apt` ]; then	# App DEBIAN
 		sudo snap install ${snapApps[$i]}
 	done
 
+	PROMPT_COMMAND="Setting Up SDDM"
 	#TODO ---- Setup sddm ----
 	if [ `systemctl status display-manager | head -n1 | awk '{print $2;}'` != 'sddm.service' ]; then
 		echo 'Pakeičiamas display manager į sddm'
@@ -58,36 +68,44 @@ if [ `which apt` ]; then	# App DEBIAN
 		sudo mv plasma-chili /usr/share/sddm/themes/
 	fi
 
-	echo 'xrandr --output DP-1 --mode 1920x1080 --rate 144' >> ~/.bashrc
-	echo 'alias xr144="xrandr --output DP-1 --mode 1920x1080 --rate 144"' >> ~/.bashrc
-	echo 'alias ..="cd .."' >> ~/.bashrc
-	echo 'eval "$(starship init bash)"' >> ~/.bashrc
-	
+	#TODO ---- Setup .bashrc ----
+	cat bashrc_config.bashrc >> ~/.bashrc
 
-# ?----------------------------------------------
-# ?|                  FEDORA                    |
-# ?----------------------------------------------
+# ?┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
+# ?│                   FEDORA                   │
+# ?┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
 elif [ `which rpm` ]; then
 
-	# Flatpak
+	#TODO ---- Post-installation necessary commands ----
+	PROMPT_COMMAND="Running Post-Installation System Updates"
+	dnf check-update
+
+	#TODO ---- Setup Flatpak ----
+	PROMPT_COMMAND="Setting Up Flatpak"
 	sudo dnf install -y flatpak
 	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-	# VS code
+	#TODO ---- Install VS Code ----
+	PROMPT_COMMAND="Installing VS Code"
 	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 	sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-	# Minecraft launcher
+	#TODO ---- Install Minecraft launcher ----
+	PROMPT_COMMAND="Setting Up Minecraft"
 	sudo dnf copr enable stenstorp/Minecraft -y
-	# Balena etcher
+	#TODO ---- Install Balena Etcher ----
+	PROMPT_COMMAND="Installing Balena Etcher"
 	sudo dnf install dnf-plugins-core dnf-utils dnf-plugin-config-manager -y
 	curl -1sLf \
    'https://dl.cloudsmith.io/public/balena/etcher/setup.rpm.sh' \
    | sudo -E bash
-   	# Lotion (notion.so)
+   	#TODO ---- Install Lotion (notion.so) ----
+	PROMPT_COMMAND="Installing Lotion"
    	wget https://raw.githubusercontent.com/puneetsl/lotion/master/setup.sh
    	chmod +x setup.sh
    	sudo ./setup.sh web
    
+	#TODO ---- Install necessary applications ----
+	PROMPT_COMMAND="Installing Necessary Applications"
 	dnfApps=(discord minecraft-launcher dotnet-sdk-6.0 balena-etcher-electron 7z vlc starship xclip valgrind)
 	flatApps=(com.spotify.Client com.discordapp.Discord com.github.IsmaelMartinez.teams_for_linux)
 	for i in ${!dnfApps[@]}
@@ -98,25 +116,19 @@ elif [ `which rpm` ]; then
 	do
 		flatpak install flathub ${flatApps[$i]}
 	done
-	
 	dnf check-update
 	sudo dnf install code
 	flatpak install flathub com.discordapp.Discord
 
-	# Setup ./bashrc
-	echo 'xrandr --output DP-1 --mode 1920x1080 --rate 144' >> ~/.bashrc
-	echo 'alias xr144="xrandr --output DP-1 --mode 1920x1080 --rate 144"' >> ~/.bashrc
-	echo 'alias ..="cd .."' >> ~/.bashrc
-	echo 'eval "$(starship init bash)"' >> ~/.bashrc
+	#TODO ---- Setup .bashrc ----
+	cat bashrc_config.bashrc >> ~/.bashrc
 
-
-
-
-# ?----------------------------------------------
-# ?|                   ARCH                     |
-# ?----------------------------------------------
+# ?┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
+# ?│                    ARCH                    │
+# ?┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
 
 elif [ `which pacman` ]; then
+	PROMPT_COMMAND="Running Post-Installation System Updates"
 	sudo pacman -Syu
 	sudo pacman -S --noconfirm build-essentials base-devel
 
@@ -130,6 +142,7 @@ elif [ `which pacman` ]; then
 	cd ${scriptLoc}
 	
 	#TODO ---- Install applications ----
+	PROMPT_COMMAND="Installing Necessary Applications"
 	yayApps=(optimus-manager optimus-manager-qt gnome-session-properties piper-git minecraft-launcher visual-studio-code-bin dotnet-sdk-bin eclipse-java teams)
 	pacApps=(nvidia vim npm gdb steam discord lutris gimp vlc qbittorrent etcher powerline xorg-xkill nvidia-prime dosbox starship neofetch xclip spotify-launcher docbook-xml intltool autoconf-archive gnome-common itstool docbook-xsl mallard-ducktype yelp-tools glib2-docs python-pygments python-anytree gtk-doc sddm)
 	for i in ${!yayApps[@]}
@@ -142,17 +155,16 @@ elif [ `which pacman` ]; then
 		sudo pacman -S --noconfirm ${pacApps[$i]}
 	done
 
-	#TODO ---- Setup react ----
-	sudo npm -g install create-react-app
-
-	#TODO ---- Install rust ----
+	#TODO ---- Install Rust ----
+	PROMPT_COMMAND="Installing Rust"
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rust.sh
 	chmod +x rust.sh
 	./rust.sh
 	source ~/.cargo/env
 	cd ${scriptLoc}
 	
-	#TODO ---- Setup sddm ----
+	#TODO ---- Setup SDDM ----
+	PROMPT_COMMAND="Setting Up SDDM"
 	if [ `systemctl status display-manager | head -n1 | awk '{print $2;}'` != 'sddm.service' ]; then
 		echo 'Pakeičiamas display manager į sddm'
 		systemctl disable `systemctl status display-manager | head -n1 | awk '{print $2;}'`
@@ -162,9 +174,11 @@ elif [ `which pacman` ]; then
 	fi
 		
 	#TODO ---- Change from hardware clock to local clock ----
+	PROMPT_COMMAND="Changing from hardware to local clock"
 	timedatectl set-local-rtc 1 --adjust-system-clock
 	
 	#TODO ---- Setup .zshrc ----
+	PROMPT_COMMAND="Updating .zshrc"
 	echo 'xrandr --output DP-1 --mode 1920x1080 --rate 144' >> ~/.zshrc
 	echo 'xrandr --output DP-1-1 --mode 1920x1080 --rate 144' >> ~/.zshrc
 	echo 'alias xr144="xrandr --output DP-1 --mode 1920x1080 --rate 144"' >> ~/.zshrc
@@ -179,14 +193,89 @@ else
 	echo "Unknown distribution"
 fi
 
-#TODO ---- Gnome configuration ----
-# Force alt + tab to switch only on current workspace in GNOME
-gsettings set org.gnome.shell.app-switcher current-workspace-only true
-# Screen time-out
-gsettings set org.gnome.desktop.session idle-delay 4500
-gsettings set org.gnome.desktop.screensaver lock-delay 900
+if [ `which gsettings` ]; then
+	#TODO ---- Gnome configuration ----
+	PROMPT_COMMAND="Setting gsettings"
+	# Force alt + tab to switch only on current workspace in GNOME
+	gsettings set org.gnome.shell.app-switcher current-workspace-only true
+	# Screen time-out
+	gsettings set org.gnome.desktop.session idle-delay 4500
+	gsettings set org.gnome.desktop.screensaver lock-delay 900
+
+	PS3="Select your prefered color for the theme: "
+
+
+	select themeColor in default purple pink red orange yellow green teal blue all
+	do
+		case $themeColor in
+			"default")
+				echo "Selected the $themeColor color"
+				colorCode="#FB8C00"
+				break;;
+			"purple")
+				echo "Selected the $themeColor color"
+				colorCode="#AB47BC"
+				break;;
+			"pink")
+				echo "Selected the $themeColor color"
+				colorCode="#EC407A"
+				break;;
+			"red")
+				echo "Selected the $themeColor color"
+				colorCode="#E53935"
+				break;;
+			"orange")
+				echo "Selected the $themeColor color"
+				colorCode="#FB8C00"
+				break;;
+			"yellow")
+				echo "Selected the $themeColor color"
+				colorCode="#FBC02D"
+				break;;
+			"green")
+				echo "Selected the $themeColor color"
+				colorCode="#4CAF50"
+				break;;
+			"teal")
+				echo "Selected the $themeColor color"
+				colorCode="#009688"
+				break;;
+			"blue")
+				echo "Selected the $themeColor color"
+				colorCode="#3684DD"
+				break;;
+			"all")
+				echo "Selected the $themeColor color"
+				colorCode="#FB8C00"
+				break;;
+			*)
+		esac
+	done
+	#TODO ---- Theme installation ----
+	PROMPT_COMMAND="Installing Graphite Theme For GNOME"
+	git clone https://github.com/vinceliuice/Graphite-gtk-theme.git
+	sudo Graphite-gtk-theme/install.sh -t $themeColor
+	sudo rm -r Graphite-gtk-theme/
+	gsettings set org.gnome.desktop.interface gtk-theme "Graphite-$themeColor-Dark"
+	gsettings set org.gnome.desktop.wm.preferences theme "Graphite-$themeColor-Dark"
+
+	#TODO ---- Icon pack installation ----
+	PROMPT_COMMAND="Installing Tela Circle Icon Pack"
+	git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
+	Tela-circle-icon-theme/install.sh $themeColor
+	sudo rm -r Tela-circle-icon-theme/
+	gsettings set org.gnome.desktop.interface icon-theme "Tela-circle-$themeColor-dark"
+
+	#TODO ---- Configuring Starship ----
+	PROMPT_COMMAND="Configuring Starship"
+	mkdir -p ~/.config && touch ~/.config/starship.toml
+	search=%COLORCODE
+	cat starship.toml > ~/.config/starship.toml
+	sed -i "s/$search/$colorCode/" ~/.config/starship.toml
+fi
 
 #TODO ---- Setup SSH ----
+PROMPT_COMMAND="Setting Up SSH With Github"
 echo 'Setting up ssh'
 echo 'Enter git email:'
 read gitEmail
@@ -196,9 +285,10 @@ echo 'SSH key copied to clipboard, go to Github:'
 echo '1. Go to user settings'
 echo '2. Press "SSH and GPG keys"'
 echo '3. Paste in the copied text in to the text box'
-read  -n 1 -p '(Press any key to continue)' input
+read -n 1 -p '(Press any key to continue)' input
 
 #TODO ---- Setup git ----
+PROMPT_COMMAND="Setting Up Git"
 echo 'Setting up git'
 echo 'Enter git username:'
 read gitName
---
+++ bashrc_config.bashrc
@@ -0,0 +1,32 @@
+xrandr --output DP-1-1 --mode 1920x1080 --rate 144 2>/dev/null
+alias xr144="xrandr --output DP-1-1 --mode 1920x1080 --rate 144"
+alias ..="cd .."
+
+function set_win_title() {
+    local cmd=" ($@)"
+    if [[ "$cmd" == " (starship_precmd)" || "$cmd" == " ()" ]]
+    then
+        cmd=""
+    fi
+    if [[ $PWD == $HOME ]]
+    then
+        if [[ $SSH_TTY ]]
+        then
+        echo -ne "\033]0;@ $HOSTNAME ~$cmd\a" < /dev/null
+        else
+        echo -ne "\033]0;~$cmd\a" < /dev/null
+        fi
+    else
+        BASEPWD=$(basename "$PWD")
+        if [[ $SSH_TTY ]]
+        then
+        echo -ne "\033]0;$BASEPWD @ $HOSTNAME $cmd\a" < /dev/null
+        else
+        echo -ne "\033]0;$BASEPWD $cmd\a" < /dev/null
+        fi
+    fi
+
+}
+starship_precmd_user_func="set_win_title"
+eval "$(starship init bash)"
+trap "$(trap -p DEBUG |  awk -F"'" '{print $2}');set_win_title \${BASH_COMMAND}" DEBUG
