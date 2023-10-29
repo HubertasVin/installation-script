218526b9a2a14e32f3feed118a53fa88c6b45e76
HubertasVin <hubertas2003@gmail.com>
2023-10-29 16:22:41 +0200

    Added colors to polybar wakatime module

--- user_config/polybar/scripts/getWakatime.py
+++ user_config/polybar/scripts/getWakatime.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
 
+def getClockColor(totalH, multiplier):
+    if totalH < 6 * multiplier:
+        return "#99FFC2"
+    elif totalH >= 6 * multiplier and 9 * multiplier > totalH:
+        return "#8DC133"
+    elif totalH >= 9 * multiplier and 12 * multiplier > totalH:
+        return "#F97B4D"
+    elif totalH >= 12 * multiplier and 14 * multiplier > totalH:
+        return "#C10B57"
+    else:
+        return "#8D0601"
+
+
+
+
 import urllib.request, json
 with urllib.request.urlopen("https://wakatime.com/share/@35061e46-e6ae-4c54-9981-c98a50bfab17/0674e617-7e65-4143-b88f-848f35e7f359.json") as url:
     data = json.load(url)
+    data = data['data']
     sumH = 0
     sumM = 0
     sumS = 0
-    for i in data['data']:
+    for i in data:
         sumS += i['grand_total']['total_seconds']
     sumH = int(round(sumS / 3600, 0))
     sumM = int(round(sumS % 3600 / 60, 0))
-    print("7 days: " + str(sumH) + ":" + str(sumM), end="")
-    print(" | today: " + data['data'][6]['grand_total']['digital'], end="")
+
+    print("%{F#E57C46}7 days%{F-}: %{F" + getClockColor(sumH, 3) + "}"  + str(sumH) + ":" + str(sumM), end="%{F-}")
+    print(" %{F#707880}|%{F-} %{F#42A5F5}today%{F-}: %{F" + getClockColor(data[6]['grand_total']['hours'], 1) + "}" + data[6]['grand_total']['digital'], end="%{F-}")
 
 with urllib.request.urlopen("https://wakatime.com/share/@35061e46-e6ae-4c54-9981-c98a50bfab17/bec79df7-baae-4ff6-afee-f47571162cf9.json") as url:
     data = json.load(url);
-    print(" | " + data['data'][0]['name'] + ": " + data['data'][0]['digital'], end="")
-    print(" | " + data['data'][1]['name'] + ": " + data['data'][1]['digital'], end="")
-    print(" | " + data['data'][2]['name'] + ": " + data['data'][2]['digital'], end="")
+    data = data['data']
+    for i in range(0, 3):
+        print(" %{F#707880}|%{F-} %{F" + data[i]['color'] + "}" + data[i]['name'] + "%{F-}: %{F" + getClockColor(data[i]['hours'], 3) + "}"  + data[i]['digital'], end="%{F-}")
