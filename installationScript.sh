cb5f480259e4029965ed995a24515c095b33ac76
HubertasVin <>
2024-04-05 22:00:00 +0300

    Modified installation file to make it more readable by splitting it in bigger sections.

--- installationScript.sh
+++ installationScript.sh
@@ -38,7 +38,8 @@ elif [ `which pacman 2>/dev/null` ]; then
 else
 	echo "Unknown distribution"
 fi
-
+#TODO ---------------------
+#TODO Package manager setup
 #TODO ---- Snapd setup ----
 sudo systemctl enable --now snapd.service
 echo 'Reboot your computer to enable snapd to function fully'
@@ -54,13 +55,15 @@ case "$answer" in
         echo 'Skipping confirmation'
         ;;
 esac
-
 #TODO ---- Load sdk function to the script ----
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
+
+
+#TODO -----------------------------
+#TODO Theme installation
+#TODO ---- Gnome configuration ----
 if [ `which gnome-shell` ]; then
-	#TODO ---- Gnome configuration ----
-	PROMPT_COMMAND="Restoring GNOME settings..."
 	cd $scriptLoc
 	dconf load -f / < user_config/saved_settings.dconf
 	# Force alt + tab to switch only on current workspace in GNOME
@@ -74,57 +77,54 @@ if [ `which gnome-shell` ]; then
 	gsettings set org.gnome.shell.keybindings toggle-overview []
 fi
 
-PS3="Select your prefered color for the theme: "
-
 select themeColor in default purple pink red orange yellow green teal blue all
 do
-        case $themeColor in
-                "default")
-                        echo "Selected the $themeColor color"
-                        colorCode="#FB8C00"
-                        break;;
-                "purple")
-                        echo "Selected the $themeColor color"
-                        colorCode="#AB47BC"
-                        break;;
-                "pink")
-                        echo "Selected the $themeColor color"
-                        colorCode="#EC407A"
-                        break;;
-                "red")
-                        echo "Selected the $themeColor color"
-                        colorCode="#E53935"
-                        break;;
-                "orange")
-                        echo "Selected the $themeColor color"
-                        colorCode="#FB8C00"
-                        break;;
-                "yellow")
-                        echo "Selected the $themeColor color"
-                        colorCode="#FBC02D"
-                        break;;
-                "green")
-                        echo "Selected the $themeColor color"
-                        colorCode="#4CAF50"
-                        break;;
-                "teal")
-                        echo "Selected the $themeColor color"
-                        colorCode="#009688"
-                        break;;
-                "blue")
-                        echo "Selected the $themeColor color"
-                        colorCode="#3684DD"
-                        break;;
-                "all")
-                        echo "Selected the $themeColor color"
-                        colorCode="#FB8C00"
-                        break;;
-                *)
-        esac
+    case $themeColor in
+        "default")
+            echo "Selected the $themeColor color"
+            colorCode="#FB8C00"
+            ;;
+        "purple")
+            echo "Selected the $themeColor color"
+            colorCode="#AB47BC"
+            ;;
+        "pink")
+            echo "Selected the $themeColor color"
+            colorCode="#EC407A"
+            ;;
+        "red")
+            echo "Selected the $themeColor color"
+            colorCode="#E53935"
+            ;;
+        "orange")
+            echo "Selected the $themeColor color"
+            colorCode="#FB8C00"
+            ;;
+        "yellow")
+            echo "Selected the $themeColor color"
+            colorCode="#FBC02D"
+            ;;
+        "green")
+            echo "Selected the $themeColor color"
+            colorCode="#4CAF50"
+            ;;
+        "teal")
+            echo "Selected the $themeColor color"
+            colorCode="#009688"
+            ;;
+        "blue")
+            echo "Selected the $themeColor color"
+            colorCode="#3684DD"
+            ;;
+        "all")
+            echo "Selected the $themeColor color"
+            colorCode="#FB8C00"
+            ;;
+        *)
+    esac
 done
 
 #TODO ---- Theme installation ----
-PROMPT_COMMAND="Installing Graphite Theme For GNOME..."
 git clone https://github.com/vinceliuice/Graphite-gtk-theme.git
 sudo Graphite-gtk-theme/install.sh -t $themeColor
 sudo rm -r Graphite-gtk-theme/
@@ -132,14 +132,25 @@ gsettings set org.gnome.desktop.interface gtk-theme "Graphite-$themeColor-Dark"
 gsettings set org.gnome.desktop.wm.preferences theme "Graphite-$themeColor-Dark"
 
 #TODO ---- Icon pack installation ----
-PROMPT_COMMAND="Installing Tela Circle Icon Pack..."
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
 Tela-circle-icon-theme/install.sh $themeColor
 sudo rm -r Tela-circle-icon-theme/
 gsettings set org.gnome.desktop.interface icon-theme "Tela-circle-$themeColor-dark"
 
+#TODO ---- Setup One Dark Pro for terminal ----
+cd ~
+git clone https://github.com/denysdovhan/one-gnome-terminal.git
+cd one-gnome-terminal/
+chmod +x one-dark.sh
+./one-dark.sh
+cd ~
+rm -rf one-gnome-terminal/
+
+
+
+#TODO ------------------------------
+#TODO Configurations
 #TODO ---- Configuring Starship ----
-PROMPT_COMMAND="Configuring Starship..."
 mkdir -p ~/.config && touch ~/.config/starship.toml
 search=%COLORCODE
 cat user_config/starship_template.toml > ~/.config/starship.toml
@@ -153,17 +164,40 @@ if [ ! -d "alacritty-theme" ]; then
 fi
 cp "$scriptLoc"/user_config/alacritty.yml ~/.config/alacritty
 
-#TODO ---- Install fonts for Polybar ----
-# STYLE='simple'
-# [[ ! -d "$FDIR" ]] && mkdir -p "$FDIR"
-# cp -rf "$scriptLoc"/fonts/* "$FDIR"
+#TODO ---- Setup ranger ----
+ranger --copy-config=rifle
+ranger --copy-config=rc
+echo "" >> ~/.config/ranger/rc.config
+echo "# Archives" >> ~/.config/ranger/rc.config
+echo "map ex extract" >> ~/.config/ranger/rc.config
+echo "map ec compress" >> ~/.config/ranger/rc.config
+mkdir -p ~/.config/ranger/plugins
+cd ~/.config/ranger/plugins
+if [ ! -d "ranger-archives" ] ; then
+    git clone https://github.com/maximtrp/ranger-archives.git
+fi
+#TODO ---- Install disk mounting plugin ----
+cd ~/.config/ranger/plugins
+if [ ! -d "ranger_udisk_menu" ]; then
+	git clone https://github.com/SL-RU/ranger_udisk_menu
+fi
+touch ../commands.py
+if [ $(grep -Fxq "from plugins.ranger_udisk_menu.mounter import mount" commands.py) ]; then
+	echo "from plugins.ranger_udisk_menu.mounter import mount" >> ../commands.py
+fi
 
-#TODO ---- Setup Polybar ----
-# cp -r "$scriptLoc"/user_config/polybar/* ~/.config/polybar
-
-#TODO ---- Setup qtile and rofi ----
-# cp -r "$scriptLoc"/user_config/qtile/* ~/.config/qtile
-# cp -r "$scriptLoc"/user_config/rofi/* ~/.config/rofi
+#TODO ---- Setup NeoVIM ----
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+mkdir -p ~/.vim
+cd ~/.vim
+mkdir -p ~/.config/nvim/
+cp -r ~/Installation_Script/user_config/nvim ~/.config/
+cp -r ~/Installation_Script/user_config/vimrcs ~/.vim/
+nvim +PlugInstall
+nvim +WakaTimeApiKey
+#TODO ---- Allow NeoVIM to access the clipboard ----
+set clipboard=unnamedplus
 
 #TODO ---- Restore configuration for terminal, tmux and bash/zsh ----
 cp "$scriptLoc"/user_config/.inputrc ~
@@ -183,80 +217,24 @@ fi
 #TODO ---- Install Spotify ----
 snap install spotify
 
-#TODO ---- Install Gradle ----
-PROMPT_COMMAND="Installing Gradle..."
-sdk install gradle
 
+
+#TODO -------------------------
+#TODO Install development tools
+#TODO ---- Install Gradle ----
+sdk install gradle
 #TODO ---- Install Rustc ----
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
 #TODO ---- Install dotnet script for running .cs files ----
 dotnet tool update -g dotnet-script
-
-# TODO ---- Add the user to pkg-build group ----
-if command -v pacman &> /dev/null
-then
-    local user=$(whoami)
-	sudo usermod -a -G pkg-build $user
-fi
-
 #TODO ---- Install GHCup ----
-# PROMPT_COMMAND="Installing GHCup..."
-# curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-
-#TODO ---- Setup One Dark Pro for terminal ----
-PROMPT_COMMAND="Setting up One Dark Pro for terminal..."
-cd ~
-git clone https://github.com/denysdovhan/one-gnome-terminal.git
-cd one-gnome-terminal/
-chmod +x one-dark.sh
-./one-dark.sh
-cd ~
-rm -rf one-gnome-terminal/
-
-#TODO ---- Setup ranger ----
-PROMPT_COMMAND="Setting up ranger..."
-ranger --copy-config=rifle
-ranger --copy-config=rc
-echo "" >> ~/.config/ranger/rc.config
-echo "# Archives" >> ~/.config/ranger/rc.config
-echo "map ex extract" >> ~/.config/ranger/rc.config
-echo "map ec compress" >> ~/.config/ranger/rc.config
-mkdir -p ~/.config/ranger/plugins
-cd ~/.config/ranger/plugins
-if [ ! -d "ranger-archives" ] ; then
-    git clone https://github.com/maximtrp/ranger-archives.git
-fi
-#TODO ---- Install disk mountingf plugin ----
-cd ~/.config/ranger/plugins
-if [ ! -d "ranger_udisk_menu" ]; then
-	git clone https://github.com/SL-RU/ranger_udisk_menu
-fi
-touch ../commands.py
-if [ $(grep -Fxq "from plugins.ranger_udisk_menu.mounter import mount" commands.py) ]; then
-	echo "from plugins.ranger_udisk_menu.mounter import mount" >> ../commands.py
-fi
-
-#TODO ---- Setup NeoVIM ----
-PROMPT_COMMAND="Setting up NeoVIM..."
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-mkdir -p ~/.vim
-cd ~/.vim
-mkdir -p ~/.config/nvim/
-cp -r ~/Installation_Script/user_config/nvim ~/.config/
-cp -r ~/Installation_Script/user_config/vimrcs ~/.vim/
-nvim +PlugInstall
-nvim +WakaTimeApiKey
-#TODO ---- Allow NeoVIM to access the clipboard ----
-set clipboard=unnamedplus
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 
 #     ---- Moving scripts to ~/tools ----
 mkdir -p ~/tools
 cp -r "$scriptLoc"/scripts/* ~/tools
 
 #TODO ---- Setup SSH ----
-PROMPT_COMMAND="Setting Up SSH With Github..."
 echo 'Setting up ssh'
 echo -n 'Enter git email: '
 read gitEmail
@@ -270,7 +248,6 @@ echo '3. Paste in the copied text in to the text box'
 read -n 1 -p '(Press any key to continue)' answer
 
 #TODO ---- Setup git ----
-PROMPT_COMMAND="Setting Up Git..."
 echo 'Setting up git'
 echo -n 'Enter git username: '
 read gitName
@@ -279,16 +256,11 @@ git config --global user.email "$gitEmail"
 echo "Host *" >> ~/.ssh/config
 echo "    StrictHostKeyChecking no" >> ~/.ssh/config
 
-PROMPT_COMMAND="Downloading BackupFolder..."
 cd ~/Documents
 git clone git@github.com:HubertasVin/BackupFolder.git
-
-PROMPT_COMMAND="Downloading CSGO Config Backup..."
-git clone git@github.com:HubertasVin/CSGO_Config.git
-
-PROMPT_COMMAND="Downloading Pictures Backup..."
+cd ~/Pictures
 git clone git@github.com:HubertasVin/PictureBackup.git
-cp -r PictureBackup/* ~/Pictures/
+mv -r PictureBackup/* .
 sudo rm -r PictureBackup/
 
 sudo cp "$scriptLoc"/desktop_shortcuts/dosbox-school.desktop /usr/share/applications/
