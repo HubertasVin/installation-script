559875b8bb3ef65cabed63909638f4ddcc48205f
HubertasVin <>
2024-05-12 20:17:50 +0300

    Backup

--- scripts/backup/backup.sh
+++ scripts/backup/backup.sh
@@ -1,72 +1,100 @@
 #! /bin/bash
-scriptLoc="$(cd $(dirname "$0"); pwd)"
+scriptLoc="$(pwd)"
 
 set -e
 
-#exec 2>/tmp/error
+touch /tmp/error
 
 handle_error() {
     local ERROR=$(cat /tmp/error)
     echo $ERROR
     echo "$0 at line $1 with command $ERROR" >> /home/hubertas/tools/tool-errors.log
+    echo "$0 at line $1 with command $ERROR" >> "$TEMP_OUTPUT"
 }
 
-trap 'handle_error $LINENO' ERR
-
-# Backup script
-currentLoc=$(pwd)
-YELLOW='\033[1;33m'
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-OKBLUE='\033[94m'
-OKGREEN='\033[92m'
-FAIL='\033[91m'
-
-Backup_Success() {
+backup_success() {
 	if git status | grep -q 'Your branch is up to date'; then
 		echo -e "${OKGREEN}Backup successful for ${PWD}${NC}"
+		echo "${OKGREEN}Backup successful for ${PWD}${NC}" >> "$1"
 	else
-		echo -e "${FAIL}Backup failed for ${PWD}${NC}"
+		echo -e "${FAIL}Branch is up to date ${PWD}${NC}"
+		echo "${FAIL}Branch is up to date ${PWD}${NC}" >> "$1"
 	fi
 }
 
-PROMPT_COMMAND="Backing up..."
+backup_commands() {
+    currentLoc=$(pwd)
+    YELLOW='\033[1;33m'
+    GREEN='\033[0;32m'
+    RED='\033[0;31m'
+    NC='\033[0m'            # No Color
+    OKBLUE='\033[94m'
+    OKGREEN='\033[92m'
+    FAIL='\033[91m'
 
-echo -e "${OKBLUE}Backing up...${NC}"
-echo -e "${OKBLUE}Copying config files${NC}"
-# Copy polybar, qtile, rofi configs
-cp -r ~/.config/polybar/* ~/Installation_Script/user_config/polybar 2>/dev/null || :
-cp -r ~/.config/qtile/* ~/Installation_Script/user_config/qtile 2>/dev/null || :
-cp -r ~/.config/rofi/* ~/Installation_Script/user_config/rofi 2>/dev/null || :
-# Copy Nvim configuration
-cp -r ~/.config/nvim/* ~/Installation_Script/user_config/nvim 2>/dev/null || :
-cp -r ~/.vim/vimrcs/* ~/Installation_Script/user_config/vimrcs 2>/dev/null || :
-# Copy inputrc
-cp ~/.inputrc ~/Installation_Script/user_config/
-# Copy TMUX configuration
-cp ~/.tmux.conf ~/Installation_Script/user_config/template.tmux.conf 2>/dev/null || :
-cp ~/.tmux.conf.local ~/Installation_Script/user_config/template.tmux.conf.local 2>/dev/null || :
-sed -i 's/^tmux_conf_theme_colour_0=.*/tmux_conf_theme_colour_0="%COLORCODE"    # default/' ~/Installation_Script/user_config/template.tmux.conf.local
-# Copy Terminator configuration
-cp -r ~/.config/terminator/* ~/Installation_Script/user_config/terminator 2>/dev/null || :
-# Copy GNOME settings
-cd ~/Installation_Script/user_config
-dconf dump / > saved_settings.dconf
+    TEMP_OUTPUT=$(mktemp)
+    TEMP_ERROR=$(mktemp)
 
-echo "Starting backup to git"
+    trap 'handle_error $LINENO $TEMP_ERROR' ERR
 
-cd "$scriptLoc"
-while read p || [[ -n $p ]];
-do
-	echo -e "${OKBLUE}Backing up ${p}${NC}"
-	cd $p
-	git add . 1> /dev/null
-	git commit -m "Backup"
-	git push
-	Backup_Success
-done < gitlocations.txt
+    echo -e "${OKBLUE}Backing up...${NC}"
+    echo "${OKBLUE}Backing up...${NC}" >> "$TEMP_OUTPUT"
+    echo -e "${OKBLUE}Copying config files${NC}"
+    echo "${OKBLUE}Copying config files${NC}" >> "$TEMP_OUTPUT"
+    PROMPT_COMMAND="Backing up..."
 
-echo -e "${OKGREEN}Backup complete${NC}"
-PROMPT_COMMAND="Completed backup."
+    # Copy polybar, qtile, rofi configs
+    cp -r ~/.config/polybar/* ~/Installation_Script/user_config/polybar 2>/dev/null || :
+    cp -r ~/.config/qtile/* ~/Installation_Script/user_config/qtile 2>/dev/null || :
+    cp -r ~/.config/rofi/* ~/Installation_Script/user_config/rofi 2>/dev/null || :
+    # Copy Nvim configuration
+    cp -r ~/.config/nvim/* ~/Installation_Script/user_config/nvim 2>/dev/null || :
+    cp -r ~/.vim/vimrcs/* ~/Installation_Script/user_config/vimrcs 2>/dev/null || :
+    # Copy inputrc
+    cp ~/.inputrc ~/Installation_Script/user_config/
+    # Copy TMUX configuration
+    cp ~/.tmux.conf ~/Installation_Script/user_config/template.tmux.conf 2>/dev/null || :
+    cp ~/.tmux.conf.local ~/Installation_Script/user_config/template.tmux.conf.local 2>/dev/null || :
+    sed -i 's/^tmux_conf_theme_colour_0=.*/tmux_conf_theme_colour_0="%COLORCODE"    # default/' ~/Installation_Script/user_config/template.tmux.conf.local
+    # Copy Terminator configuration
+    cp -r ~/.config/terminator/* ~/Installation_Script/user_config/terminator 2>/dev/null || :
+    # Copy GNOME settings
+    dconf dump / > ~/Installation_Script/user_config/saved_settings.dconf
+
+    echo "Starting backup to git"
+    echo "Starting backup to git" >> "$TEMP_OUTPUT"
+
+    while read p || [[ -n $p ]];
+    do
+        cd $p
+        pwd
+        echo -e "${OKBLUE}Backing up ${p}${NC}"
+        echo "${OKBLUE}Backing up ${p}${NC}" >> "$TEMP_OUTPUT"
+        git add . 1> /dev/null
+        if ! git diff-index --quiet HEAD --; then
+            git commit -m "Backup"
+            git push
+        fi
+        backup_success "$TEMP_OUTPUT"
+        tput clear
+        echo -e "$(cat "$TEMP_OUTPUT")"
+    done < gitlocations.txt
+
+    echo -e "${OKGREEN}Backup complete${NC}"
+    echo "${OKGREEN}Backup complete${NC}" >> "$TEMP_OUTPUT"
+    PROMPT_COMMAND="Completed backup."
+
+    echo "Press any key to continue..."
+    read -n 1 -s
+}
+
+HANDLE_ERROR_SERIALIZED=$(declare -f handle_error)
+BACKUP_SUCCESS_SERIALIZED=$(declare -f backup_success)
+BACKUP_COMMANDS_SERIALIZED=$(declare -f backup_commands)
+
+echo 'Still here'
+
+tmux new-session -d -s backup-session bash -c "$BACKUP_COMMANDS_SERIALIZED; $HANDLE_ERROR_SERIALIZED; $BACKUP_SUCCESS_SERIALIZED; cd /home/hubertas/tools/backup; backup_commands"
+tmux set-option -t backup-session status off
+TMUX='' tmux attach -t backup-session > /dev/null
 
