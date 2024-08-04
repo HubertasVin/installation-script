a2d7cd1fa197b0d15a3e2bff051bd0d28fbbd46f
HubertasVin <>
2024-08-04 16:24:06 +0300

    UPDATE: Added nitrogen support in i3.

--- scripts/backup/backup.sh
+++ scripts/backup/backup.sh
@@ -47,12 +47,12 @@ backup_commands() {
     cp -r ~/.config/polybar/* ~/Installation_Script/user_config/polybar 2>/dev/null || :
     cp -r ~/.config/qtile/* ~/Installation_Script/user_config/qtile 2>/dev/null || :
     cp ~/.local/share/rofi/themes/rounded-nord-dark.rasi ~/Installation_Script/user_config/rofi/themes 2>/dev/null || :
-    cp ~/.local/bin/rofi-power-menu ~/Installation_Script/user_config/rofi/
+    cp ~/.local/bin/rofi-power-menu ~/Installation_Script/user_config/rofi/ 2>/dev/null || :
     # Copy i3 and i3blocks configs
-    cp -r ~/.config/i3/ ~/Installation_Script/user_config/
-    cp -r ~/.config/i3blocks/ ~/Installation_Script/user_config/
+    cp -r ~/.config/i3/ ~/Installation_Script/user_config/ 2>/dev/null || :
+    cp -r ~/.config/i3blocks/ ~/Installation_Script/user_config/ 2>/dev/null || :
     # Copy Nvim config
-    cp -rf ~/.config/nvim/lua/* ~/Installation_Script/user_config/nvim
+    cp -rf ~/.config/nvim/lua/* ~/Installation_Script/user_config/nvim 2>/dev/null || :
     # cp -r ~/.config/nvim/* ~/Installation_Script/user_config/nvim 2>/dev/null || :
     # cp -r ~/.vim/vimrcs/* ~/Installation_Script/user_config/vimrcs 2>/dev/null || :
     # Copy inputrc
