c3bb228bd49b42d49c02fbf1a80e9f18d1f80732
HubertasVin <>
2024-09-23 18:09:16 +0300

    UPDATE: Added autorandr config setup for external monitor while only one monitor active.

--- setup-autorandr.sh
+++ setup-autorandr.sh
@@ -3,11 +3,17 @@
 echo -e "Plug in all displays to setup autorandr"
 read -n 1 -p '(Press any key to continue)' answer
 
-xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 --rotate normal \
-       --output DP-1 --off
+# Setup autorandr config for only Laptop
+xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 --rotate normal
 autorandr --save laptop
 sed -i '/^DP-1/d' ~/.config/autorandr/laptop/setup
 
+# Setup autorandr config for only external only in dual display mode
 xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rate 144 --rotate normal \
        --output eDP-1 --off
 autorandr --save external
+
+# Setup autorandr config for only external only in single display mode
+xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rate 144 --rotate normal
+autorandr --save external-single
+sed -i '/^eDP-1/d' ~/.config/autorandr/external-single/setup
