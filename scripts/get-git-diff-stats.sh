aad745ee9164bb5ca36e903a9b166d5b47ad9aec
HubertasVin <hubertas.personal@proton.me>
2026-08-03 14:21:31 +0300

    Updating git status script to output variables.

--- scripts/get-git-diff-stats.sh
+++ scripts/get-git-diff-stats.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
+shortstat=$(git diff --shortstat 2>/dev/null)
+untracked=$(git ls-files --others --exclude-standard 2>/dev/null | wc -l | tr -d ' ')
 
-stats=$(git diff --shortstat 2>/dev/null)
+awk -v S="$shortstat" -v U="$untracked" '
+BEGIN {
+    files = added = deleted = 0
 
-awk -v B="$(tput bold)" \
-	-v TF="$(tput setaf 142)" \
-	-v AF="$(tput setaf 70)"  \
-	-v DF="$(tput setaf 124)" \
-	-v UF="$(tput setaf 205)" \
-	-v R="$(tput sgr0)" \
-	-v U="$(git ls-files --others --exclude-standard 2>/dev/null | wc -l | tr -d ' ')" \
-	'BEGIN {
-    split("'"$stats"'", a)
-    files    = (a[1] + 0) + (U + 0)
-    tracked  = (a[1] + 0)
-    add      = (a[4] + 0)
-    del      = (a[6] + 0)
+    if (match(S, /([0-9]+) insertion/, m))         added   = m[1] + 0
+    if (match(S, /([0-9]+) deletion/, m))          deleted = m[1] + 0
+    if (match(S, /([0-9]+) file[s]? changed/, m))  files   = m[1] + 0
 
-    if (files > 0) {
-        if (tracked > 0)  printf " %s%s✓%d%s", B, TF, tracked, R
-        if (U > 0)        printf " %s%s?%d%s", B, UF, U, R
-        if (add > 0)      printf " %s%s↑%d%s", B, AF, add, R
-        if (del > 0)      printf " %s%s↓%d%s", B, DF, del, R
-    }
+    printf "export POSH_GIT_TRACKED=%d\n",   files
+    printf "export POSH_GIT_UNTRACKED=%d\n", U + 0
+    printf "export POSH_GIT_ADDED=%d\n",     added
+    printf "export POSH_GIT_DELETED=%d\n",   deleted
 }'
+
